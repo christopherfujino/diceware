0a79d881ef08ce43d3e90594cba778dfb7b71b0b
Christopher Fujino <christopherfujino@gmail.com>
2026-02-18 20:12:48 -0800

    fix build

--- main.go
+++ main.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 
-	"github.com/christopherfujino/go-pass/diceware"
+	"github.com/christopherfujino/diceware/diceware"
 )
+
+func getAverageGuessCountString(entropy int) string {
+	var entropyFloat = (math.Pow(2, float64(entropy))) / 2
+	var rec func(int) string = nil
+	rec = func(i int) string {
+		if i < 1000 {
+			return fmt.Sprintf("%03d", i)
+		}
+		var prefix = i / 1000
+		var suffix = i - (prefix * 1000)
+		var prefixString = ""
+		if prefix < 1000 {
+			prefixString = fmt.Sprintf("%d", prefix)
+		} else {
+			prefixString = rec(prefix)
+		}
+		return fmt.Sprintf("%s,%s", prefixString, rec(suffix))
+	}
+	return rec(int(entropyFloat))
+}
 
 func main() {
 	var n = getN(os.Args)
 	password := diceware.GeneratePassword(n)
 	var entropy = diceware.CalculateEntropy(n)
 	fmt.Printf(
-		"%s\n\nThe length of your password is %d with %d bits of entropy.\n",
+		"%s\n\nThe length of your password is %d bytes with %d bits of entropy (on average %s guesses).\n",
 		password,
 		len(password),
 		entropy,
+		getAverageGuessCountString(entropy),
 	)
 }
 
-func getN(args []string) (int) {
+func getN(args []string) int {
 	if len(args) > 1 {
 		var nString = args[1]
 		n, err := strconv.Atoi(nString)
@@ -34,52 +56,3 @@ func getN(args []string) (int) {
 	}
 	return 4
 }
-
-//type ParseState int
-//
-//const (
-//	ParseNum ParseState = iota
-//	ParseWord
-//)
-//
-//func Words() WordList {
-//	const path = "./wordlist.txt"
-//	var words = make(map[int]string, 8000)
-//	var state = ParseNum
-//
-//	bytes, err := os.ReadFile(path)
-//	if err != nil {
-//		panic(err)
-//	}
-//	var buffer = strings.Builder{}
-//	var num = 0
-//	var ch byte
-//	// we assume wordlist is ASCII
-//	for i := range len(bytes) {
-//		ch = bytes[i]
-//
-//		switch state {
-//		case ParseNum:
-//			if ch == '\t' {
-//				state = ParseWord
-//			} else {
-//				digit, err := strconv.Atoi(string(ch))
-//				if err != nil {
-//					panic(err)
-//				}
-//				// Base 6 digits
-//				num = num*6 + (digit - 1)
-//			}
-//		case ParseWord:
-//			if ch == '\n' {
-//				words[num] = buffer.String()
-//				buffer.Reset()
-//				num = 0
-//				state = ParseNum
-//			} else {
-//				buffer.WriteByte(ch)
-//			}
-//		}
-//	}
-//	return words
-//}
